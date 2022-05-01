219d6f986ed1f8fdd67ef3b5e6c8b06ff84dd10e
Madison Kendall <lilyzetakovic@gmail.com>
2022-05-01 17:25:09 -0600

    wrote category routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -1,30 +1,51 @@
-
-
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
-
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+// this route is to GET all categories
+router.get("/", (req, res) => {
+  Category.findAll({ include: [Product] })
+  .then((data) => res.json(data))
+  .catch((error) => res.json(error));
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+// this route is to GET a category by id
+router.get("/:id", (req, res) => {
+  Category.findOne(req.params.id, { include: [Product] })
+  .then((data) => res.json(data))
+  .catch((error) => res.json(error));
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+// this post route is to CREATE a new category
+router.post("/", (req, res) => {
+  Category.create(req.body)
+  .then((data) => res.json(data))
+  .catch((error) => res.json(error));
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+// this route will UPDATE a category by its `id` value
+router.put("/:id", (req, res) => {
+  Category.update({
+    id: req.body.id,
+    category_name: req.body.category_name,
+  },
+{
+  where: {
+    id: req.params.id,
+  },
+})
+.then((data) => res.json(data))
+.catch((error) => res.json(error));
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+// this route is to DELETE a category by its `id` value
+router.delete("/:id", (req, res) => {
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((data) => res.json(data))
+  .catch((error) => res.json(error));
 });
 
 module.exports = router;
